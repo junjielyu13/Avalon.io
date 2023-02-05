8bf74846bd4a8fb9b0b8f0da3afe568846cdfe40
Junjie_Li <jfjunjieli@gmail.com>
2023-02-05 03:26:15 +0100

    config: nodemon

--- src/service/app.service.ts
+++ src/service/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'dssdsdsd adsdsds d ello word';
+    return 'dssdsdsd sds adsdsds d ello word';
   }
 }
