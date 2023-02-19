d5d7f1647229eb1e60242f5f7e95cf54c842fe04
Junjie_Li <jfjunjieli@gmail.com>
2023-02-19 04:00:35 +0100

    bug: gatwayes error

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './controller/app.controller';
 import { AppService } from './service/app.service';
 import { ApiModule } from './modules/api.module';
 import { PrismaModule } from './modules/prisma.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [ApiModule, PrismaModule],
+  imports: [ApiModule, PrismaModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
